52a3f848afd291769ebb2dd327220bdda6f1a588
Sneha Khandelwal <sneha0401>
2020-08-25 20:00:41 +0530

    updated test.cu

--- cuda-neural-network/src/layers/test.cu
+++ cuda-neural-network/src/layers/test.cu
@@ -24,28 +24,44 @@ __global__ void softmax(float* input, float* buffer, float* max_num, int Z_x_dim
 int main()
 {
 	
-	float Z[25*25];
+	float Z[25][25];
 	int i ,j;
  	for( i = 0; i < 25*25; ++i){
-  		Z[i] = rand();
+ 		for( j =0; j < 25; ++j){
+  			Z[i][j] = rand();
+    	}
     }
 
-  	float buffer[25*25];
+  	float buffer[25][25];
 
  	for( i = 0; i < 25*25; ++i){
- 		std::cout<<Z[i]<<'\t';
-   		std::cout<<'\n';
- 	}
+ 		for( j =0; j < 25; ++j){
+  			buffer[i][j] = rand();
+    	}
+    }
 
  	float *max_num = new float[25];
 
- 	softmax<<<2, 20>>>(Z, buffer, max_num, 25, 25);
+ 	float Z_d, buffer_d, max_num_d;
+ 	cudaMalloc((void **)&Z_d, 25*25*sizeof(float));
+ 	cudaMalloc((void **)&buffer_d, 25*25*sizeof(float));
+ 	cudaMalloc((void **)&max_num_d, 25*sizeof(float));
+
+ 	cudaMemcpy(Z_d, Z, 25*25*sizeof(float), cudaMemcpyHostToDevice);
+ 	cudaMemcpy(buffer_d, buffer, 25*25*sizeof(float), cudaMemcpyHostToDevice);
+ 	cudaMemcpy(max_num_d, max_num, 25*sizeof(float), cudaMemcpyHostToDevice);
+
+ 	dim3 block_size(64);
+	dim3 num_of_blocks((25 * 25 + block_size.x - 1) / block_size.x);
+
+ 	softmax<<<num_of_blocks, block_size>>>(Z_d, buffer_d, max_num_d, 25, 25);
 /*
  	for(i = 0; i < 10; i++){
  		for( j = 0; j < 3; ++j)
     		std::cout<<buffer[i][j]<<'\t';
 	}
 */
+ 	cudaMemcpy(max_num, max_num_d, 25*sizeof(float), cudaMemcpyDeviceToHost);
  	for(i = 0; i < 25; i++){
  		std::cout<<max_num[i]<<std::endl;
  	}
