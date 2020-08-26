eda67bddc70d86de1d535ec6060d48d046d756c7
Sneha Khandelwal <sneha0401>
2020-08-26 12:48:58 +0530

    updated test.cu

--- cuda-neural-network/src/layers/test.cu
+++ cuda-neural-network/src/layers/test.cu
@@ -6,7 +6,8 @@
 __global__ void softmax(float* input, float* buffer, float* max_num, int Z_x_dim, int Z_y_dim) {
   	assert(input);
 
-	int row = blockIdx.y * blockDim.y + threadIdx.y;
+	int row = blockIdx.x * blockDim.x + threadIdx.x;
+	//printf("%d \n", row);
 	//int col = blockIdx.x * blockDim.x + threadIdx.x;
   	// assert(input_len >= 0);  Not needed
 	for (int i = 0; i < Z_y_dim; i++) {
@@ -14,30 +15,28 @@ __global__ void softmax(float* input, float* buffer, float* max_num, int Z_x_dim
 	}
 
 	if (row < Z_y_dim ) {
-		for (int i = 0; i < Z_x_dim; i++) {
-			if(max_num[i] < input[row * Z_x_dim + i] ){
+		for (int i = 0; i < Z_x_dim; i++){ 
+			if(max_num[row] < input[row * Z_x_dim + i] ){
 				max_num[row] = input[row * Z_x_dim + i];
 			}
+			
 		}
 	}
 }
+
 int main()
 {
 	
-	float Z[25][25];
+	float Z[25*25];
 	int i ,j;
  	for( i = 0; i < 25*25; ++i){
- 		for( j =0; j < 25; ++j){
-  			Z[i][j] = rand();
-    	}
+		Z[i] = rand();
     }
 
-  	float buffer[25][25];
+  	float buffer[25*25];
 
  	for( i = 0; i < 25*25; ++i){
- 		for( j =0; j < 25; ++j){
-  			buffer[i][j] = rand();
-    	}
+  		buffer[i] = rand();
     }
 
  	float *max_num = new float[25];
